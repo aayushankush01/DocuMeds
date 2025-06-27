aede9376d8cb7fa4345b084dd5e02c50c9b2e1ec
aayushankush01 <155513481+aayushankush01@users.noreply.github.com>
2025-06-27 21:11:03 +0530

    Update Data_Base.py

--- app/Data_Base.py
+++ app/Data_Base.py
@@ -1,8 +1,7 @@
-import os
 from datetime import datetime
 from flask import Flask, render_template, request
 import mysql.connector as m
-mydatabase=m.connect(host="localhost",user="root",password=os.getenv("MYSQL_PASSWORD"),database="projectdb1")
+mydatabase=m.connect(host="localhost",user="root",password="My Password",database="projectdb1") #Replace My Password with your SQL Password
 cursor=mydatabase.cursor()
 app=Flask(__name__)
 def table_format(rows):
@@ -32,7 +31,6 @@ def submit():
         val=(name,sex,age,presmed,inhousemed,investigations,his)
         cursor.execute(qs,val)
         mydatabase.commit()
-        # return f"Data for {name} added successfully!"
         return render_template("AfterPost.html",name=name)
 
 @app.route("/get",methods=['GET'])
@@ -41,15 +39,11 @@ def get():
     start_date=start_date+" 00:00:00"
     end_date=request.args.get('end_date')
     end_date=end_date+" 23:59:59"
-    #date_format_str = '%%d/%%m/%%y--%%h:%%i:%%s %%p'
-    #qg=f'''SELECT DATE_FORMAT(Date_Time,'{date_format_str}') as Formatted_Date_Time,patientname,sex,age,Prescribed_Medicine,Inhouse_Medicine,Investigations,His FROM P1 WHERE Date_Time BETWEEN %s AND %s'''
     qg = "SELECT Date_Time,patientname,sex,age,Prescribed_Medicine,Inhouse_Medicine,Investigations,His FROM P1 WHERE Date_Time BETWEEN %s AND %s"
     cursor.execute(qg,(start_date,end_date))
     print(end_date)
     rows=cursor.fetchall()
- #   column=cursor.column_names
     column,data=table_format(rows)
-    #print(repr(rows[0]['His']))
     return render_template("AllData_Table.html",data=data,columns=column)
 
 @app.route("/search",methods=['GET'])
@@ -57,18 +51,22 @@ def search():
     Search_name=request.args.get('Search_name')
     Search_name_Og=Search_name
     Search_name=Search_name.lower()
+    #Command to only get records of patient with name Search_name
     search_query="Select Date_Time,patientname,sex,age,Prescribed_Medicine,Inhouse_Medicine,Investigations,His From p1 where LOWER(patientname) = %s "
     cursor.execute(search_query,(Search_name,))
     rows=cursor.fetchall()
     column,data=table_format(rows)
+    # Gives latest age as takes the age from first row of the pateint searched
     age_query="SELECT age FROM p1 ORDER BY Sr_No DESC LIMIT 1"
     cursor.execute(age_query)
-    age=cursor.fetchone()
+    age=cursor.fetchone() #Latest age stored as tuple in variable age
+    # Store age from tuple age to variable age as an integer
     age=age[0]
+    # Fetch previously entered sex
     sex_query="SELECT sex FROM p1 ORDER BY Sr_No DESC LIMIT 1"
     cursor.execute(sex_query)
-    sex=cursor.fetchone()
-    sex=sex[0]
+    sex=cursor.fetchone() #Store sex of patient as tuple
+    sex=sex[0] #Store sex from tuple sex to variable sex as a string
     return render_template("SearchData_Table.html",data=data,columns=column,Search_name_Og=Search_name_Og,age=age,sex=sex)
 
 
